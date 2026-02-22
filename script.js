f8ad1de86abe5b1fbd3c3f967b4a5348ed3f65a7
Hunter-Coder20 <theepicbossofgaming@gmail.com>
2026-02-22 09:44:36 -0700

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
 function contact(event) {
-    event.preventDefault();
-    emailjs
-        .sendForm(
-            'service_25xt23g',
-            'template_3er5edf',
-            event.target,
-            'kGC0CCkJDKlk1szsH',
-    ).then(() => {
-        console.log('this worked')
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+
+  emailjs
+    .sendForm(
+      "service_25xt23g",
+      "template_3er5edf",
+      event.target,
+      "kGC0CCkJDKlk1szsH",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on theepicbossofgaming@gmail.com"
+        );
     })
+
+  loading.classList += " modal__overlay--visible";
+  setTimeout(() => {}, 100);
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
